16860026dc6c25c2c62dd59d23369ffbc0c581c2
wangxiangyubuaichiyu <3067094013@qq.com>
2024-05-10 19:26:58 +0800

    qss，只差player界面了，当然home和user在服务器写之前不会设置

--- Widget_Main.cpp
+++ Widget_Main.cpp
@@ -1,8 +1,12 @@
 #include "Widget_Main.h"
 #include "ui_widget_main.h"
 
+#include <QEvent>
 #include <QFile>
 #include <QMouseEvent>
+#include <QPainter>
+#include <QPainterPath>
+#include <QPalette>
 #include <QPixmap>
 #include <QScreen>
 #include <qDebug>
@@ -14,7 +18,11 @@ Widget_Main::Widget_Main(QWidget *parent)
     ui->setupUi(this);
 
     setWindowFlags(Qt::FramelessWindowHint);        // 隐藏标题栏和边框
+    setMouseTracking(true);                         // 启用鼠标跟踪
+    ui->search->setMouseTracking(true);             //当有多个控件的时候需要所有控件都要设置跟踪，否则不好使
+    ui->Title_bar->setMouseTracking(true);
     on_btn_big_clicked();                           //窗口放中间
+    qApp->installEventFilter(this);                 //为所有控件添加事件过滤器->为了搜索框
     QFile file(":/style.qss");
     file.open(QFile::ReadOnly | QFile::Text);
     QTextStream in(&file);
@@ -53,13 +61,42 @@ Widget_Main::~Widget_Main()
     delete ui;
 }
 
-void Widget_Main::SLT_show(QImage img)
+QPixmap Widget_Main::roundImage(const QPixmap &src, int radius)
 {
-    QPixmap pixmap = QPixmap::fromImage(img);// 使用 QImage 创建 QPixmap
-    pixmap = pixmap.scaled(ui->lb_palyer->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation); // 缩放图片以适应 QLabel 的大小
+    if (src.isNull()) return QPixmap();
 
-    ui->lb_palyer->setPixmap(pixmap); // 设置 QLabel 的 pixmap 属性
-    ui->lb_palyer->update(); // 更新 QLabel 以显示新的 pixmap
+    // 如果图片尺寸小于圆角矩形的尺寸，不进行修改
+    if (src.width() <= radius * 2 && src.height() <= radius * 2)
+        return src;
+
+    // 创建目标图片
+    QPixmap rounded(src.size());
+    rounded.fill(Qt::transparent);
+
+    // 创建Painter
+    QPainter painter(&rounded);
+    painter.setRenderHint(QPainter::Antialiasing);
+
+    // 创建圆角路径，只保留左上角和右上角->有时间可以实现一下
+//    QPainterPath path;
+//    path.moveTo(src.width(), src.height());
+//    path.lineTo(src.width(), 0);
+//    path.arcTo(QRectF(0, 0, radius * 2, radius * 2), 90.0, -90.0);
+//    path.lineTo(src.width() - radius, src.height());
+//    path.arcTo(QRectF(src.width() - radius * 2, 0, radius * 2, radius * 2), 0.0, -90.0);
+//    path.lineTo(0, src.height());
+//    path.closeSubpath();
+    // 创建圆角路径
+    QPainterPath path;
+    path.addRoundedRect(rounded.rect(), radius, radius);
+
+    // 设置剪裁区域
+    painter.setClipPath(path);
+    painter.setRenderHints(QPainter::Antialiasing);         //抗锯齿
+    painter.setRenderHints(QPainter::SmoothPixmapTransform);//平滑像素图变换
+    painter.drawPixmap(rounded.rect(), src);
+
+    return rounded;
 }
 
 void Widget_Main::mousePressEvent(QMouseEvent *event)
@@ -74,12 +111,33 @@ void Widget_Main::mousePressEvent(QMouseEvent *event)
 
 void Widget_Main::mouseMoveEvent(QMouseEvent *event)
 {
+    //移动窗口
     if (m_dragging)
     {
         // 计算窗口的新位置
         QPoint newpos = event->globalPosition().toPoint() - m_offset;
         move(newpos);
     }
+
+    //搜索框进行变色
+    // 获取鼠标的位置
+    QPoint mousePos = event->pos();
+    // 判断鼠标是否在指定矩形区域内
+    if (mousePos.x() >= 600 && mousePos.x() <= 1020 &&
+        mousePos.y() >= 20 && mousePos.y() <= 72)
+    {
+        // 进入指定区域，改变状态并更新界面
+        inSpecifiedArea = true;
+        update();
+    }
+    else
+    {
+        // 离开指定区域，改变状态并更新界面
+        inSpecifiedArea = false;
+        update();
+    }
+
+    QWidget::mouseMoveEvent(event);
 }
 
 void Widget_Main::mouseReleaseEvent(QMouseEvent *event)
@@ -88,6 +146,44 @@ void Widget_Main::mouseReleaseEvent(QMouseEvent *event)
     {
         m_dragging = false;
     }
+}
+
+void Widget_Main::paintEvent(QPaintEvent *event)
+{
+    // 根据鼠标是否在指定区域内绘制不同的颜色
+    if (inSpecifiedArea)
+    {
+        ui->btn_search->setStyleSheet("background-color: white;color: black;");
+        ui->btn_search->setIcon(QIcon(":/image/search1.png")); // 设置按钮图标
+    }
+    else
+    {
+        ui->btn_search->setStyleSheet(" background-color: rgba(0, 0, 0, 0);color: white;");
+        ui->btn_search->setIcon(QIcon(":/image/search2.png")); // 设置按钮图标
+    }
+
+    QWidget::paintEvent(event);
+}
+
+bool Widget_Main::eventFilter(QObject *watched, QEvent *event)
+{
+    if(event->type() == QEvent::MouseButtonPress && watched !=  ui->let_search)
+    {
+        ui->let_search->setFocus();
+        ui->let_search->clearFocus();
+        this->setFocus();
+    }
+    return QObject::eventFilter(watched,event);//千万不能返回true
+}
+
+void Widget_Main::SLT_show(QImage img)
+{
+    QPixmap pixmap = QPixmap::fromImage(img);// 使用 QImage 创建 QPixmap
+    pixmap = pixmap.scaled(ui->lb_palyer->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation); // 缩放图片以适应 QLabel 的大小
+    // 将原始图片转换为圆角图片
+    QPixmap roundedPixmap = roundImage(pixmap, 20); // 20为圆角半径
+    ui->lb_palyer->setPixmap(roundedPixmap); // 设置 QLabel 的 pixmap 属性
+    ui->lb_palyer->update(); // 更新 QLabel 以显示新的 pixmap
 }
 
 void Widget_Main::on_btn_close_clicked()
@@ -153,4 +249,3 @@ void Widget_Main::on_btn_open_clicked()
     play->SetFilePath("E:\\Documents\\01.mp4");
     play->start();
 }
-
--- Widget_Main.h
+++ Widget_Main.h
@@ -16,11 +16,13 @@ class Widget_Main : public QWidget
 public:
     Widget_Main(QWidget *parent = nullptr);
     ~Widget_Main();
+    QPixmap roundImage(const QPixmap &src, int radius);// 函数用于绘制圆角的图片
 protected:
-    //拖动窗口可以进行拖拽
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
     void mouseReleaseEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
+    bool eventFilter(QObject *watched, QEvent *event) override;
 private slots:
     void SLT_show(QImage img);   //展示到控件上
     void on_btn_close_clicked(); //关闭窗口
@@ -32,11 +34,13 @@ private slots:
     void on_btn_play_clicked(); //用户页
     void on_btn_open_clicked(); //播放与暂停按钮
 
+
 private:
-    Ui::MainWindow *ui;  //ui界面
-    AVPlay* m_play;      //播放器
-    QPoint m_offset;     //记录指针点击坐标
-    bool m_dragging = false;
+    Ui::MainWindow *ui;                 //ui界面
+    AVPlay* m_play;                     //播放器
+    QPoint m_offset;                    //记录指针点击坐标
+    bool m_dragging = false;            //标记鼠标是否在标记位置移动窗口
+    bool inSpecifiedArea = false;       // 标记是否在指定区域内
 
     //HomeWindow homeWnd;
 
